078b4874cd74f3ccef56796939e3d89a92f82570
Jyoti Kumari <btera7330@gmail.com>
2024-12-01 19:13:56 +0530

    more chnages

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello0000, World!"}
+    return {"message": "Hello0000, Worldddddd!"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int,q:Union[str,None]=None):
